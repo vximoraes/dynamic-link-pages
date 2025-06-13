fef749fd372fb4b477185a1656f8cbe5a8a2c2ab
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2025-06-13 14:28:29 -0400

    feat: refactor app initialization and user information handling

--- src/main.ts
+++ src/main.ts
@@ -4,24 +4,37 @@ import { inserirInformacoes } from './ui/inserirInformacoes'
 import { mensagemSemId, mensagemUsuarioNaoEncontrado } from './ui/mensagens'
 
 const params = new URLSearchParams(window.location.search)
-const id = params.get("id")
-
-const imagem = document.querySelector<HTMLImageElement>('#imagem')!
-const nome = document.getElementById('nome')!
-const linksContainer = document.getElementById('links-container')!
-const qrcode = document.querySelector<HTMLImageElement>('#qrcode')!
-const imagemContainer = document.getElementById('imagem-container')!
-const divNome = document.getElementById('div-nome')!
-const qrcodeContainer = document.getElementById('qrcode-container')!
+let id = params.get("id")
 
 if (!id) {
-    mensagemSemId(imagemContainer, divNome, linksContainer, qrcodeContainer)
+    window.location.replace(window.location.pathname + '?id=1');
 } else {
-    inserirInformacoesComVerificacao()
+    iniciarApp(id)
 }
 
-async function inserirInformacoesComVerificacao() {
-    const usuario = await buscarUsuario(id!)
+function iniciarApp(id: string) {
+    const imagem = document.querySelector<HTMLImageElement>('#imagem')!
+    const nome = document.getElementById('nome')!
+    const linksContainer = document.getElementById('links-container')!
+    const qrcode = document.querySelector<HTMLImageElement>('#qrcode')!
+    const imagemContainer = document.getElementById('imagem-container')!
+    const divNome = document.getElementById('div-nome')!
+    const qrcodeContainer = document.getElementById('qrcode-container')!
+
+    inserirInformacoesComVerificacao(id, imagem, nome, linksContainer, qrcode, imagemContainer, divNome, qrcodeContainer)
+}
+
+async function inserirInformacoesComVerificacao(
+    id: string,
+    imagem: HTMLImageElement,
+    nome: HTMLElement,
+    linksContainer: HTMLElement,
+    qrcode: HTMLImageElement,
+    imagemContainer: HTMLElement,
+    divNome: HTMLElement,
+    qrcodeContainer: HTMLElement
+) {
+    const usuario = await buscarUsuario(id)
     if (!usuario) {
         mensagemUsuarioNaoEncontrado(imagemContainer, divNome, linksContainer, qrcodeContainer)
         return
